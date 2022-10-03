24af3655192226de33d34c717a767d2f85711622
KelvinQiu <kelvinqiu802@outlook.com>
2022-10-03 21:02:37 +0800

    feat: useDrag

--- src/components/Draggable.jsx
+++ src/components/Draggable.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 import { useDrag } from 'react-dnd';
 
 function Draggable({ left, top }) {
-  return <div style={{ position: 'absolute', left, top }}>draggable</div>;
+  const [{ isDragging }, drag] = useDrag(
+    () => ({
+      type: 'box',
+      item: { left, top },
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+    }),
+    [top, left]
+  );
+
+  return (
+    <div style={{ ...styles, left, top }} ref={drag}>
+      draggable
+    </div>
+  );
 }
+
+const styles = {
+  position: 'absolute',
+  padding: '5px',
+  border: '1px solid orange',
+};
 
 export default Draggable;
