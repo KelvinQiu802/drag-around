9e20538bfa4ccb0c84264634e9f83d484fb16a92
KelvinQiu <kelvinqiu802@outlook.com>
2022-10-03 21:06:09 +0800

    feat: useDrop

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className='App'>
-      <Droppable>
+      <Droppable setPosition={setPosition}>
         <Draggable top={position.top} left={position.left} />
       </Droppable>
     </div>
--- src/components/Droppable.jsx
+++ src/components/Droppable.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { useDrop } from 'react-dnd';
 
-function Droppable({ children }) {
-  return <div className='droppable'>{children}</div>;
+function Droppable({ children, setPosition }) {
+  const [, drop] = useDrop(() => ({
+    accept: 'box',
+    drop(item, monitor) {
+      const delta = monitor.getDifferenceFromInitialOffset();
+      const left = Math.round(item.left + delta.x);
+      const top = Math.round(item.top + delta.y);
+      setPosition({
+        top,
+        left,
+      });
+    },
+  }));
+
+  return (
+    <div className='droppable' ref={drop}>
+      {children}
+    </div>
+  );
 }
 
 export default Droppable;
