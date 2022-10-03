62049ccd5974c9de933640cdef8a34d80d743f82
KelvinQiu <kelvinqiu802@outlook.com>
2022-10-03 20:52:13 +0800

    fix: typo position

--- src/components/Draggable.jsx
+++ src/components/Draggable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDrag } from 'react-dnd';
 
 function Draggable({ left, top }) {
-  return <div style={{ posotion: 'absolute', left, top }}>draggable</div>;
+  return <div style={{ position: 'absolute', left, top }}>draggable</div>;
 }
 
 export default Draggable;
